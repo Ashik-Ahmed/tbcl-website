8743172efb05af46a1efe9e2295948f00a739391
ashik-ahmed <ashikahmed121@gmail.com>
2025-02-04 14:12:27 +0600

    send-email api route created and subject input field added in contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -121,6 +121,21 @@ export default function EnhancedContactUs() {
                                         />
                                     </div>
                                     <div>
+                                        <label htmlFor="subject" className="block text-sm font-medium text-gray-700">
+                                            Subject
+                                        </label>
+                                        <input
+                                            type="subject"
+                                            name="subject"
+                                            id="subject"
+                                            autoComplete="subject"
+                                            required
+                                            value={formData.subject}
+                                            onChange={handleChange}
+                                            className="mt-1 py-2 block w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm transition duration-300 ease-in-out"
+                                        />
+                                    </div>
+                                    <div>
                                         <label htmlFor="message" className="block text-sm font-medium text-gray-700">
                                             Message
                                         </label>
